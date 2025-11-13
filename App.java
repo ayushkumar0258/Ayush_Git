b502c1d07f69302bfc9c2a7eba806c549cd41a9f
ayushkumar0258 <112791228+ayushkumar0258@users.noreply.github.com>
2025-11-13 22:16:44 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package ayush0258;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
