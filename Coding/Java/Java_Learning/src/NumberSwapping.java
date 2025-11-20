be5b49241c737a2bbe4bc8eabf29cbca25a7bfc3
ayushkumar0258 <ayushkumar0258@gmail.com>
2025-11-21 00:21:23 +0200

    adding new changes and python file

--- Coding/Java/Java_Learning/src/NumberSwapping.java
+++ Coding/Java/Java_Learning/src/NumberSwapping.java
@@ -5,19 +5,23 @@ class NumberSwapping
     public static void main (String[] args)
     {
         // Create a Scanner object to read input
+        // This Java code snippet is a program that swaps the values of two numbers entered by the
+        // user. Here is a breakdown of what each part of the code does:
         Scanner scanner = new Scanner(System.in); 
         System.out.println("Enter first number: ");
+	System.out.println("New file check");
         int num1 = scanner.nextInt(); // Read first number
         System.out.println("Enter second number: ");
         int num2 = scanner.nextInt(); // Read second number     
         System.out.println("Before Swapping: num1 = " + num1 + ", num2 = " + num2);
         // Swapping numbers using a temporary variable
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
+        num1=num1+num2;
+        num2 = num1-num2;
+        
+        num1 = num1-num2;
         System.out.println("After Swapping: num1 = " + num1 + ", num2 = " + num2);
         // Close the scanner
-        scanner.close(); 
+        scanner.close();
         ///// thanks!   
 	//// good boss	
     /// vaibhav
---
+++ Coding/Python/ayush.py
@@ -0,0 +1,6 @@
+a=7
+b=9
+print (a+b)
+print ("my name is Ayush Kumar")
+print ("I am learning python")  
+print ("I love coding")
